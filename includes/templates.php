87d85080588de14d2eacf7bf33c434c35a54b7d8
helbetica-com-ar <helbetica@outlook.com>
2025-05-29 19:37:13 -0300

    added markup '.years' for style purposes

--- includes/templates.php
+++ includes/templates.php
@@ -221,7 +221,7 @@ function render_step_2() {
                     </div>
                     
                     <div class="rate-info">
-                        <div class="rate-item">
+                        <div class="rate-item years">
                             <span class="label"><?php echo esc_html(__('Loan Term:', 'custom-mortgage-calculator')); ?></span>
                             <span class="value" id="selected-loan-term">-</span> <?php echo esc_html(__('years', 'custom-mortgage-calculator')); ?>
                         </div>
